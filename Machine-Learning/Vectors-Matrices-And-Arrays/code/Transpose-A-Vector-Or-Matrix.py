18410b62f5e9a34bfa659a3dfad6d977462b1101
Offliner <42777325+Offliners@users.noreply.github.com>
2020-01-17 00:19:58 +0800

    Create Transpose-A-Vector-Or-Matrix.py

---
+++ Machine-Learning/Vectors-Matrices-And-Arrays/code/Transpose-A-Vector-Or-Matrix.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+vector = np.array([1, 2, 3, 4, 5, 6])
+
+matrix = np.array([[1, 2, 3],
+                   [4, 5, 6],
+                   [7, 8, 9]])
+
+print(vector.T)
+print(matrix.T)
