1ccb8b13ebe0772303fbee8fd1c81821786dd9b0
Offliner <42777325+Offliners@users.noreply.github.com>
2020-01-17 21:00:15 +0800

    Update Make-Simulated-Data-For-Clustering.py

--- Machine-Learning/Basic/code/Make-Simulated-Data-For-Clustering.py
+++ Machine-Learning/Basic/code/Make-Simulated-Data-For-Clustering.py
@@ -1,1 +1,11 @@
+from sklearn.datasets import make_blobs
+import matplotlib.pyplot as plt
 
+X,y = make_blobs(n_samples=200,
+                 n_features=2,
+                 centers=3,
+                 cluster_std=0.5,
+                 shuffle=True)
+
+plt.scatter(X[:,0],X[:,1])
+plt.show()
