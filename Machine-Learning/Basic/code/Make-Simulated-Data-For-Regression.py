f3b87a76e3f6a7e312264fc6ce65d5d7e2e8eae3
Offliner <42777325+Offliners@users.noreply.github.com>
2020-01-17 21:07:32 +0800

    Update Make-Simulated-Data-For-Regression.py

--- Machine-Learning/Basic/code/Make-Simulated-Data-For-Regression.py
+++ Machine-Learning/Basic/code/Make-Simulated-Data-For-Regression.py
@@ -1,1 +1,13 @@
+import pandas as pd
+from sklearn.datasets import make_regression
 
+features, output, coef = make_regression(n_samples=100,
+                                         n_features=3,
+                                         n_informative=2,
+                                         n_targets=1,
+                                         noise=0.0,
+                                         coef=True)
+
+print(pd.DataFrame(features, columns=['Store 1', 'Store 2', 'Store 3']).head())
+print(pd.DataFrame(output, columns=['Sales']).head())
+print(pd.DataFrame(coef, columns=['True Coefficient Values']))
