828f73b66e7641e57eb9d20d903e7f083d93e20f
Offliner <42777325+Offliners@users.noreply.github.com>
2020-01-17 21:22:22 +0800

    Update Saving-Machine-Learning-Models.py

--- Machine-Learning/Basic/code/Saving-Machine-Learning-Models.py
+++ Machine-Learning/Basic/code/Saving-Machine-Learning-Models.py
@@ -1,1 +1,22 @@
+from sklearn.linear_model import LogisticRegression
+from sklearn import datasets
+import pickle
+from sklearn.externals import joblib
 
+iris = datasets.load_iris()
+
+X,y = iris.data, iris.target
+
+clf = LogisticRegression(random_state=0)
+clf.fit(X,y)
+
+saved_model = pickle.dumps(clf)
+
+print(saved_model)
+
+clf_from_pickle = pickle.loads(saved_model)
+print(clf_from_pickle.predict(X))
+
+joblib.dump(clf, 'Saved_model.pkl')
+clf_from_joblib = joblib.load('Saved_model.pkl')
+print(clf_from_joblib.predict(X))
