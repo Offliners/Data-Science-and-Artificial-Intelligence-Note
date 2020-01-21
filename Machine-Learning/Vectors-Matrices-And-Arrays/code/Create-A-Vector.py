1601575839efe5c6ed7df0f9490611d809a6b779
Offliner <42777325+Offliners@users.noreply.github.com>
2020-01-21 22:06:44 +0800

    Create Create-A-Vector.py

---
+++ Machine-Learning/Vectors-Matrices-And-Arrays/code/Create-A-Vector.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+vector_row = np.array([1, 2, 3])
+
+print(vector_row)
+
+vector_column = np.array([[1],
+                          [2],
+                          [3]])
+
+print(vector_column)
