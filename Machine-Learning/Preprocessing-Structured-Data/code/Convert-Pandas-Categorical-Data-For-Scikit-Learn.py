02224904d9772f329eafd44441ea6b0dfba214a7
Offliner <42777325+Offliners@users.noreply.github.com>
2020-01-24 23:10:53 +0800

    Update Convert-Pandas-Categorical-Data-For-Scikit-Learn.py

--- Machine-Learning/Preprocessing-Structured-Data/code/Convert-Pandas-Categorical-Data-For-Scikit-Learn.py
+++ Machine-Learning/Preprocessing-Structured-Data/code/Convert-Pandas-Categorical-Data-For-Scikit-Learn.py
@@ -1,1 +1,19 @@
+from sklearn import preprocessing 
+import pandas as pd
 
+raw_data = {'patient' : [1, 1, 1, 2, 2],
+            'obs' : [1, 2, 3, 1, 2],
+            'treatment' : [0, 1, 0, 1, 0],
+            'score' : ['strong', 'weak', 'normal', 'weak', 'strong']}
+
+df = pd.DataFrame(raw_data, columns=['patient', 'obs', 'treatment', 'score'])
+
+le = preprocessing.LabelEncoder()
+
+le.fit(df['score'])
+
+print(list(le.classes_))
+
+print(le.transform(df['score']))
+
+print(list(le.inverse_transform([2, 2, 1])))
