2df8fec4e292b4571907f66b064bc4e03f460a7e
Offliner <42777325+Offliners@users.noreply.github.com>
2020-01-24 23:35:49 +0800

    Update Delete-Observations-With-Missing-Values.py

--- Machine-Learning/Preprocessing-Structured-Data/code/Delete-Observations-With-Missing-Values.py
+++ Machine-Learning/Preprocessing-Structured-Data/code/Delete-Observations-With-Missing-Values.py
@@ -1,1 +1,9 @@
+import numpy as np
 
+X = np.array([[1.1, 11.1], 
+              [2.2, 22.2], 
+              [3.3, 33.3], 
+              [4.4, 44.4], 
+              [np.nan, 55]])
+
+print(X[~np.isnan(X).any(axis=1)])
