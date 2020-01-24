88c3968954992f6b423171098813078078229782
Offliner <42777325+Offliners@users.noreply.github.com>
2020-01-24 23:36:52 +0800

    Update Deleting-Missing-Values.py

--- Machine-Learning/Preprocessing-Structured-Data/code/Deleting-Missing-Values.py
+++ Machine-Learning/Preprocessing-Structured-Data/code/Deleting-Missing-Values.py
@@ -1,1 +1,9 @@
+import numpy as np
 
+X = np.array([[1, 2], 
+              [6, 3], 
+              [8, 4], 
+              [9, 5], 
+              [np.nan, 4]])
+
+print(X[~np.isnan(X).any(axis=1)])
