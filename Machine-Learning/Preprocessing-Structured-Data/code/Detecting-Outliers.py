f039738ef095029a875be082eed425675b507ae7
Offliner <42777325+Offliners@users.noreply.github.com>
2020-02-01 21:45:41 +0800

    Update Detecting-Outliers.py

--- Machine-Learning/Preprocessing-Structured-Data/code/Detecting-Outliers.py
+++ Machine-Learning/Preprocessing-Structured-Data/code/Detecting-Outliers.py
@@ -1,1 +1,16 @@
+import numpy as np
+from sklearn.covariance import EllipticEnvelope
+from sklearn.datasets import make_blobs
 
+X, _ = make_blobs(n_samples=10,
+                  n_features=2,
+                  centers=1,
+                  random_state=1)
+
+X[0,0] = 10000
+X[0,1] = 10000
+
+outliner_detector = EllipticEnvelope(contamination=.1)
+outliner_detector.fit(X)
+
+print(outliner_detector.predict(X))
