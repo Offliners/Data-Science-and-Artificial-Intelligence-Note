2a54f4ed224945dcf1683da44d797ffdda3f2b4b
Offliner <42777325+Offliners@users.noreply.github.com>
2020-02-16 21:06:52 +0800

    Update Encoding-Ordinal-Categorical-Features.py

--- Machine-Learning/Preprocessing-Structured-Data/code/Encoding-Ordinal-Categorical-Features.py
+++ Machine-Learning/Preprocessing-Structured-Data/code/Encoding-Ordinal-Categorical-Features.py
@@ -1,1 +1,13 @@
+import pandas as pd
 
+df = pd.DataFrame({'Score' : ['Low', 'Low', 'Medium', 'Medium', 'High']})
+
+print(df)
+
+scale_mapper = {'Low' : 1,
+                'Medium' : 2,
+                'High' : 3}
+
+df['Scale'] = df['Score'].replace(scale_mapper)
+
+print(df)
