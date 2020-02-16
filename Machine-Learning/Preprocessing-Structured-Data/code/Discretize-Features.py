b653531010606520fff0505d644a465221162888
Offliner <42777325+Offliners@users.noreply.github.com>
2020-02-16 21:03:15 +0800

    Update Discretize-Features.py

--- Machine-Learning/Preprocessing-Structured-Data/code/Discretize-Features.py
+++ Machine-Learning/Preprocessing-Structured-Data/code/Discretize-Features.py
@@ -1,1 +1,14 @@
+from sklearn.preprocessing import Binarizer
+import numpy as np
 
+age = np.array([[6],
+                [12],
+                [20],
+                [36],
+                [65]])
+
+binarizer = Binarizer(18)
+
+print(binarizer.fit_transform(age))
+
+print(np.digitize(age, bins=[20,30,64]))
