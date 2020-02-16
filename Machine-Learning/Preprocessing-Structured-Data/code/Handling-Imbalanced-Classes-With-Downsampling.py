47ec1719d44b0379e6e3ced3b99f203d0b9c29e6
Offliner <42777325+Offliners@users.noreply.github.com>
2020-02-16 21:13:02 +0800

    Update Handling-Imbalanced-Classes-With-Downsampling.py

--- Machine-Learning/Preprocessing-Structured-Data/code/Handling-Imbalanced-Classes-With-Downsampling.py
+++ Machine-Learning/Preprocessing-Structured-Data/code/Handling-Imbalanced-Classes-With-Downsampling.py
@@ -18,5 +18,5 @@ i_class1 = np.where(y == 1)[0]
 n_class0 = len(i_class0)
 n_class1 = len(i_class1)
 
-i_class_downsampled = np.random.choice(i_class1, size=n_class0, replace=False)
-print(np.hstack((y[i_class0], y[i_class_downsampled])))
+i_class1_downsampled = np.random.choice(i_class1, size=n_class0, replace=False)
+print(np.hstack((y[i_class0], y[i_class1_downsampled])))
