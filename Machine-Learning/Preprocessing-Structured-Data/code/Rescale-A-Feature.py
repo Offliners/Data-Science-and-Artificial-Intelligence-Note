7b83e52083e63dee78c8d0c75d2d5a6902ef90ff
Offliner <42777325+Offliners@users.noreply.github.com>
2020-02-18 22:53:55 +0800

    Update Rescale-A-Feature.py

--- Machine-Learning/Preprocessing-Structured-Data/code/Rescale-A-Feature.py
+++ Machine-Learning/Preprocessing-Structured-Data/code/Rescale-A-Feature.py
@@ -1,1 +1,12 @@
+from sklearn import preprocessing
+import numpy as np
 
+x = np.array([[-500.5], 
+              [-100.1], 
+              [0], 
+              [100.1], 
+              [900.9]])
+
+minmax_scale = preprocessing.MinMaxScaler(feature_range=(0,1))
+x_scale = minmax_scale.fit_transform(x)
+print(x_scale)
