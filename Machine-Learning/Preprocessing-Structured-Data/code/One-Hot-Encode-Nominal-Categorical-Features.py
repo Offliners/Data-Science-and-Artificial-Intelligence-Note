8fc17fc0952c0cbe3fa3b53ee5bb43a776931719
Offliner <42777325+Offliners@users.noreply.github.com>
2020-02-18 22:39:34 +0800

    Update One-Hot-Encode-Nominal-Categorical-Features.py

--- Machine-Learning/Preprocessing-Structured-Data/code/One-Hot-Encode-Nominal-Categorical-Features.py
+++ Machine-Learning/Preprocessing-Structured-Data/code/One-Hot-Encode-Nominal-Categorical-Features.py
@@ -1,1 +1,15 @@
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import OneHotEncoder
 
+x = np.array([['Texas'], 
+              ['California'], 
+              ['Texas'], 
+              ['Delaware'], 
+              ['Texas']])
+
+one_hot = OneHotEncoder()
+one_hot.fit_transform(x)
+
+print(one_hot.categories_)
+print(pd.get_dummies(x[:, 0]))
