749323fa50c8c4fba768f753bc51d943b68d6b57
Offliner <42777325+Offliners@users.noreply.github.com>
2020-02-18 22:34:06 +0800

    Update Normalizing-Observations.py

--- Machine-Learning/Preprocessing-Structured-Data/code/Normalizing-Observations.py
+++ Machine-Learning/Preprocessing-Structured-Data/code/Normalizing-Observations.py
@@ -1,1 +1,11 @@
+from sklearn.preprocessing import Normalizer
+import numpy as np
 
+X = np.array([[0.5, 0.5], 
+              [1.1, 3.4], 
+              [1.5, 20.2], 
+              [1.63, 34.4], 
+              [10.9, 3.3]])
+
+normalizer = Normalizer(norm='l2')
+print(normalizer.transform(X))
