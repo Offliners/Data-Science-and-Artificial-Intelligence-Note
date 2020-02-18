bd25b0bca66bb22309e0d4cb6af128f1e0ba768a
Offliner <42777325+Offliners@users.noreply.github.com>
2020-02-18 22:55:54 +0800

    Update Standardize-A-Feature.py

--- Machine-Learning/Preprocessing-Structured-Data/code/Standardize-A-Feature.py
+++ Machine-Learning/Preprocessing-Structured-Data/code/Standardize-A-Feature.py
@@ -1,1 +1,12 @@
+from sklearn import preprocessing
+import numpy as np
 
+x = np.array([[-500.5], 
+              [-100.1], 
+              [0], 
+              [100.1], 
+              [900.9]])
+
+scaler = preprocessing.StandardScaler()
+standardized = scaler.fit_transform(x)
+print(standardized)
