d1c64f76b2586f89c3ce444ee9726c24de140e99
Offliner <42777325+Offliners@users.noreply.github.com>
2020-02-18 22:36:24 +0800

    Update One-Hot-Encode-Features-With-Multiple-Labels.py

--- Machine-Learning/Preprocessing-Structured-Data/code/One-Hot-Encode-Features-With-Multiple-Labels.py
+++ Machine-Learning/Preprocessing-Structured-Data/code/One-Hot-Encode-Features-With-Multiple-Labels.py
@@ -1,1 +1,12 @@
+from sklearn.preprocessing import MultiLabelBinarizer
+import numpy as np
 
+y = [('Texas', 'Florida'), 
+     ('California', 'Alabama'), 
+     ('Texas', 'Florida'), 
+     ('Delware', 'Florida'), 
+     ('Texas', 'Alabama')]
+
+one_hot = MultiLabelBinarizer()
+print(one_hot.fit_transform(y))
+print(one_hot.classes_)
