115a9c833dffee5b033e0900e04a89f13a5da6b6
Offliner <42777325+Offliners@users.noreply.github.com>
2020-02-18 22:51:24 +0800

    Update Preprocessing-Iris-Data.py

--- Machine-Learning/Preprocessing-Structured-Data/code/Preprocessing-Iris-Data.py
+++ Machine-Learning/Preprocessing-Structured-Data/code/Preprocessing-Iris-Data.py
@@ -1,1 +1,18 @@
+from sklearn import datasets
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 
+iris = datasets.load_iris()
+
+X = iris.data
+y = iris.target
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+sc = StandardScaler()
+sc.fit(X_train)
+X_train_std = sc.transform(X_train)
+X_test_std = sc.transform(X_test)
+
+print(X_test[0:5])
+print(X_test_std[0:5])
