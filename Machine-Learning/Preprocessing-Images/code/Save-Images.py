bd9b5e56ebdf4433dcc40b6558cca64584508329
Offliner <42777325+Offliners@users.noreply.github.com>
2020-02-20 15:12:15 +0800

    Update Save-Images.py

--- Machine-Learning/Preprocessing-Images/code/Save-Images.py
+++ Machine-Learning/Preprocessing-Images/code/Save-Images.py
@@ -1,1 +1,11 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
 
+image = cv2.imread('images/NTNUME.png', cv2.IMREAD_GRAYSCALE)
+
+plt.imshow(image, cmap='gray')
+plt.axis('off')
+plt.show()
+
+cv2.imwrite('images/NTNUME_new.png', image)
