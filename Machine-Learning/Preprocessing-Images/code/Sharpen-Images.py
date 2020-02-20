78eb3806623693decb2f63fab46593791e616b9d
Offliner <42777325+Offliners@users.noreply.github.com>
2020-02-20 15:14:32 +0800

    Update Sharpen-Images.py

--- Machine-Learning/Preprocessing-Images/code/Sharpen-Images.py
+++ Machine-Learning/Preprocessing-Images/code/Sharpen-Images.py
@@ -1,1 +1,15 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
 
+image = cv2.imread('images/NTNUME.png', cv2.IMREAD_GRAYSCALE)
+
+kernel = np.array([[0, -1, 0], 
+                   [-1, 5,-1], 
+                   [0, -1, 0]])
+
+image_sharp = cv2.filter2D(image, -1, kernel)
+
+plt.imshow(image_sharp, cmap='gray')
+plt.axis('off')
+plt.show()
