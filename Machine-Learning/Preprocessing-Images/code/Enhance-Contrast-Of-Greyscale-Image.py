319d2b03a9ad6938661f0ba8bce1b9472f881d99
Offliner <42777325+Offliners@users.noreply.github.com>
2020-02-20 14:14:22 +0800

    Update Enhance-Contrast-Of-Greyscale-Image.py

--- Machine-Learning/Preprocessing-Images/code/Enhance-Contrast-Of-Greyscale-Image.py
+++ Machine-Learning/Preprocessing-Images/code/Enhance-Contrast-Of-Greyscale-Image.py
@@ -1,1 +1,11 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
 
+image = cv2.imread('images/NTNUME.png', cv2.IMREAD_GRAYSCALE)
+
+image_enhanced = cv2.equalizeHist(image)
+
+plt.imshow(image_enhanced, cmap='gray')
+plt.axis('off')
+plt.show()
