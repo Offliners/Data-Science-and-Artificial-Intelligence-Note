4bd2cd365f817613ad503dd40ef2245c120cc6dc
Offliner <42777325+Offliners@users.noreply.github.com>
2020-02-20 15:08:19 +0800

    Update Remove-Backgrounds.py

--- Machine-Learning/Preprocessing-Images/code/Remove-Backgrounds.py
+++ Machine-Learning/Preprocessing-Images/code/Remove-Backgrounds.py
@@ -1,1 +1,28 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+image_bgr = cv2.imread('images/NTNUME.png')
+image_rgb = cv2.cvtColor(image_bgr, cv2.COLOR_BGR2RGB)
+
+rectangle = (0, 56, 256, 150)
+
+mask = np.zeros(image_rgb.shape[:2], np.uint8)
+bgdModel = np.zeros((1, 65), np.float64)
+fgdModel = np.zeros((1, 65), np.float64)
+
+cv2.grabCut(image_rgb,
+            mask,
+            rectangle,
+            bgdModel,
+            fgdModel,
+            5,
+            cv2.GC_INIT_WITH_RECT)
+
+mask_2 = np.where((mask==2) | (mask==0), 0, 1).astype('uint8')
+image_rgb_nobg = image_rgb * mask_2[:, :, np.newaxis]
+
+plt.imshow(image_rgb_nobg)
+plt.axis('off')
+plt.show()
 
