3c424b02c492ec8c20e6446c1d101b7a518a7d4e
Offliner <42777325+Offliners@users.noreply.github.com>
2020-02-20 14:01:32 +0800

    Update Cropping-Images.py

--- Machine-Learning/Preprocessing-Images/code/Cropping-Images.py
+++ Machine-Learning/Preprocessing-Images/code/Cropping-Images.py
@@ -1,1 +1,11 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
 
+image = cv2.imread('images/NTNUME.png', cv2.IMREAD_GRAYSCALE)
+
+image_cropped = image[:,:126]
+
+plt.imshow(image_cropped, cmap='gray')
+plt.axis('off')
+plt.show()
