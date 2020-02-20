73f0c41ec7178fcb1efb8945b5c6dcf2e28b5946
Offliner <42777325+Offliners@users.noreply.github.com>
2020-02-20 15:22:34 +0800

    Update Using-Mean-Color-As-A-Feature.py

--- Machine-Learning/Preprocessing-Images/code/Using-Mean-Color-As-A-Feature.py
+++ Machine-Learning/Preprocessing-Images/code/Using-Mean-Color-As-A-Feature.py
@@ -1,1 +1,13 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
 
+image_bgr = cv2.imread('images/NTNUME.png', cv2.IMREAD_COLOR)
+
+channels = cv2.mean(image_bgr)
+observation = np.array([(channels[2], channels[1], channels[0])])
+print(observation)
+
+plt.imshow(observation)
+plt.axis('off')
+plt.show()
