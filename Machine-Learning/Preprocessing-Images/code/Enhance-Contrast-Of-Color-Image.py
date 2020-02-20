6d0798f15c9b5da28f44c05c0f91b5015bc6ed0c
Offliner <42777325+Offliners@users.noreply.github.com>
2020-02-20 14:10:08 +0800

    Update Enhance-Contrast-Of-Color-Image.py

--- Machine-Learning/Preprocessing-Images/code/Enhance-Contrast-Of-Color-Image.py
+++ Machine-Learning/Preprocessing-Images/code/Enhance-Contrast-Of-Color-Image.py
@@ -1,1 +1,13 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
 
+image_bgr = cv2.imread('images/NTNUME.png')
+
+image_yuv = cv2.cvtColor(image_bgr, cv2.COLOR_BGR2YUV)
+image_yuv[:, :, 0] = cv2.equalizeHist(image_yuv[:, :, 0])
+image_rgb = cv2.cvtColor(image_yuv, cv2.COLOR_YUV2RGB)
+
+plt.imshow(image_rgb)
+plt.axis('off')
+plt.show()
