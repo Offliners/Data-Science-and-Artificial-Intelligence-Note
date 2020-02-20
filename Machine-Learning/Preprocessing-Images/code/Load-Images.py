03a42e98b6b923ecb3c406f4c9466c231a57cb04
Offliner <42777325+Offliners@users.noreply.github.com>
2020-02-20 14:51:44 +0800

    Update Load-Images.py

--- Machine-Learning/Preprocessing-Images/code/Load-Images.py
+++ Machine-Learning/Preprocessing-Images/code/Load-Images.py
@@ -6,3 +6,13 @@ image = cv2.imread('images/NTNUME.png', cv2.IMREAD_GRAYSCALE)
 plt.imshow(image, cmap='gray')
 plt.axis('off')
 plt.show()
+
+image_bgr = cv2.imread('images/NTNUME.png', cv2.IMREAD_COLOR)
+image_rgb = cv2.cvtColor(image_bgr, cv2.COLOR_BGR2RGB)
+
+plt.imshow(image_rgb)
+plt.axis('off')
+plt.show()
+
+print(image)
+print(image.shape)
